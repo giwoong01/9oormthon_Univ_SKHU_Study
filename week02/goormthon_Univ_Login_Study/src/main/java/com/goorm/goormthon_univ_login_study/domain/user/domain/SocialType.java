ab761c66d6f00dcb9eeb9503e50783c5c451e3e7
Junyeong-An <2117ab@gmail.com>
2024-08-23 17:32:54 +0900

    Feat: 도메인 생성

---
+++ week02/goormthon_Univ_Login_Study/src/main/java/com/goorm/goormthon_univ_login_study/domain/user/domain/SocialType.java
@@ -0,0 +1,5 @@
+package com.goorm.goormthon_univ_login_study.domain.user.domain;
+
+public enum SocialType {
+    NAVER, GOOGLE
+}
---
+++ week02/goormthon_Univ_Login_Study/src/main/java/com/goorm/goormthon_univ_login_study/domain/user/domain/User.java
@@ -0,0 +1,63 @@
+package com.goorm.goormthon_univ_login_study.domain.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Builder
+@Table(name = "USERS")
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+    private String password;
+    private String nickname;
+
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    private String socialId;
+
+    private String refreshToken;
+
+    public void authorizeUser() {
+        this.role = Role.USER;
+    }
+
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+
+    public void updateNickname(String updateNickname) {
+        this.nickname = updateNickname;
+    }
+
+
+    public void updatePassword(String updatePassword, PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(updatePassword);
+    }
+
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
+    }
+}

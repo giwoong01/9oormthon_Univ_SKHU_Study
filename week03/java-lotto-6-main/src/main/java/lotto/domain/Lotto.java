252e96f04d53555f7c0592ddb0bbf9b722491f5b
Junyeong-An <2117ab@gmail.com>
2024-09-05 16:37:23 +0900

    Refactor: 필요없는 공백 및 문자 제거

--- week03/java-lotto-6-main/src/main/java/lotto/domain/Lotto.java
+++ week03/java-lotto-6-main/src/main/java/lotto/domain/Lotto.java
@@ -6,8 +6,6 @@ import java.util.List;
 public class Lotto {
     private final List<Integer> numbers;
 
-    private static final int MIN_SIZE = 6;
-
     public Lotto(List<Integer> numbers) {
         LottoValidator.validate(numbers);
         this.numbers = numbers;
@@ -16,8 +14,6 @@ public class Lotto {
     public List<Integer> getNumbers() {
         return numbers;
     }
-
-
 
     public int countMatchNumber(List<Integer> winningNumbers) {
         return (int) numbers.stream().filter(winningNumbers::contains).count();

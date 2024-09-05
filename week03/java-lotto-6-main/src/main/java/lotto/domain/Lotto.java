2b881682611ba0e4909517369bc908ee6fd63bd0
Junyeong-An <2117ab@gmail.com>
2024-09-05 16:25:27 +0900

    Feat: 로또 Validator 클래스 분리

--- week03/java-lotto-6-main/src/main/java/lotto/domain/Lotto.java
+++ week03/java-lotto-6-main/src/main/java/lotto/domain/Lotto.java
@@ -9,7 +9,7 @@ public class Lotto {
     private static final int MIN_SIZE = 6;
 
     public Lotto(List<Integer> numbers) {
-        validate(numbers);
+        LottoValidator.validate(numbers);
         this.numbers = numbers;
     }
 
@@ -17,19 +17,7 @@ public class Lotto {
         return numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != MIN_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-        }
-        if (numbers.stream().distinct().count() != MIN_SIZE) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
-        }
-        for (int number : numbers) {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
-            }
-        }
-    }
+
 
     public int countMatchNumber(List<Integer> winningNumbers) {
         return (int) numbers.stream().filter(winningNumbers::contains).count();

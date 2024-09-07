77a7d9668e86fb4ff5b0cdfbf6c43ecfab901225
Junyeong-An <2117ab@gmail.com>
2024-09-07 16:00:09 +0900

    Feat: 중복 숫자 테스트코드 추가

--- week03/java-lotto-6-main/src/test/java/lotto/LottoTest.java
+++ week03/java-lotto-6-main/src/test/java/lotto/LottoTest.java
@@ -18,4 +18,12 @@ class LottoTest {
                 .hasMessageContaining("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
 
+    @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void createLottoByDuplicatedNumber() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 로또 번호는 중복될 수 없습니다.");
+    }
+
 }

f983ed5697a27cc935b3970f3f9411dc5464b2b9
Junyeong-An <2117ab@gmail.com>
2024-09-07 16:29:14 +0900

    Refactor: 출력값 형식 수정

--- week03/java-lotto-6-main/src/main/java/lotto/view/OutputView.java
+++ week03/java-lotto-6-main/src/main/java/lotto/view/OutputView.java
@@ -17,11 +17,17 @@ public class OutputView {
     public void printResults(Map<Prize, Integer> results, double earningsRate) {
         System.out.println("당첨 통계");
         System.out.println("---");
-        for (Prize prize : Prize.values()) {
-            if (prize != Prize.NONE) {
-                System.out.println(prize.getDescription() + " (" + prize.getPrizeMoney() + "원) - " + results.get(prize) + "개");
-            }
-        }
+
+        System.out.println("3개 일치 (" + format(Prize.FIFTH.getPrizeMoney()) + "원) - " + results.get(Prize.FIFTH) + "개");
+        System.out.println("4개 일치 (" + format(Prize.FOURTH.getPrizeMoney()) + "원) - " + results.get(Prize.FOURTH) + "개");
+        System.out.println("5개 일치 (" + format(Prize.THIRD.getPrizeMoney()) + "원) - " + results.get(Prize.THIRD) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (" + format(Prize.SECOND.getPrizeMoney()) + "원) - " + results.get(Prize.SECOND) + "개");
+        System.out.println("6개 일치 (" + format(Prize.FIRST.getPrizeMoney()) + "원) - " + results.get(Prize.FIRST) + "개");
+
         System.out.printf("총 수익률은 %.1f%%입니다.%n", earningsRate);
+    }
+
+    private String format(int amount) {
+        return String.format("%,d", amount);
     }
 }

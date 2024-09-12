fe064fb1d5dfe5380d193a727047005dd960b15e
Junyeong-An <2117ab@gmail.com>
2024-09-13 04:12:28 +0900

    feat: 프로젝트 생성

---
+++ week04/gootmthon-mini-project/src/test/java/com/goorm/miniproject/GootmthonMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.miniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GootmthonMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

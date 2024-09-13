1b3cc1e6c70acaae6df57d4859f272e8c5c3035e
Kang Duoh <kduoh99@gmail.com>
2024-09-13 18:45:08 +0900

    ✨ Feat: SwaggerConfig 설정

--- week04/duoh/everytime/src/main/java/com/goormthon/everytime/global/config/SecurityConfig.java
+++ week04/duoh/everytime/src/main/java/com/goormthon/everytime/global/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.goormthon.everytime.global.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,7 +16,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 

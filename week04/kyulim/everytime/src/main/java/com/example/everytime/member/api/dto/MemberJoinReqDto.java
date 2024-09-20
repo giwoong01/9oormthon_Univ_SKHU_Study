cae5262e7475eec7a1e9e5cfab61aa33331789bc
ckr8305 <ckr8305@naver.com>
2024-09-20 17:49:31 +0900

    feat: MemberJoinReqDto 유효성 검사 추가

--- week04/kyulim/everytime/src/main/java/com/example/everytime/member/api/dto/MemberJoinReqDto.java
+++ week04/kyulim/everytime/src/main/java/com/example/everytime/member/api/dto/MemberJoinReqDto.java
@@ -1,15 +1,41 @@
 package com.example.everytime.member.api.dto;
 
 import com.example.everytime.School.domain.School;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record MemberJoinReqDto(
+        @NotBlank(message = "입학연도가 공백일 수 없습니다.")
         int year,
+
+        @NotBlank(message = "학교명이 공백일 수 없습니다.")
         School school,
+
+        @NotBlank(message = "이름이 공백일 수 없습니다.")
         String name,
+
+        @NotBlank(message = "닉네임이 공백일 수 없습니다.")
         String nickName,
+
+        @Email
+        @NotBlank(message = "이메일이 공백일 수 없습니다.")
         String email,
+
+        @NotBlank(message = "아이디가 공백일 수 없습니다.")
         String id,
+
+        @NotBlank(message = "비밀번호가 공백일 수 없습니다.")
+        @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
+                message = "비밀번호를 다시 확인해주세요")
         String password,
+
+        @NotBlank(message = "확인 비밀번호가 공백일 수 없습니다.")
         String checkPassword
 ) {
+        public MemberJoinReqDto {
+                if (!password.equals(checkPassword)) {
+                        throw new IllegalArgumentException("비밀번호를 다시 확인해주세요");
+                }
+        }
 }

6f5530301ab327359deb3ee7048c7127c71e8b3e
ckr8305 <ckr8305@naver.com>
2024-09-27 16:38:42 +0900

    docs: MemberController에 Swagger 문서화 추가

--- week04/kyulim/everytime/src/main/java/com/example/everytime/member/api/MemberController.java
+++ week04/kyulim/everytime/src/main/java/com/example/everytime/member/api/MemberController.java
@@ -7,6 +7,11 @@ import com.example.everytime.member.api.dto.reqeust.MemberLoginReqDto;
 import com.example.everytime.member.api.dto.response.MemberInfoResDto;
 import com.example.everytime.member.api.dto.response.MemberLoginResDto;
 import com.example.everytime.member.application.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,12 +25,22 @@ import java.security.Principal;
 public class MemberController {
     private final MemberService memberService;
 
+    @Operation(summary = "회원가입", description = "회원가입")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공 !"),
+            @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(example = "INVALID_HEADER or INVALID_TOKEN")))
+    })
     @PostMapping("/join")
     public RspTemplate<String> join(@RequestBody @Valid MemberJoinReqDto memberJoinReqDto) {
         memberService.join(memberJoinReqDto);
         return new RspTemplate<>(HttpStatus.CREATED, "회원가입");
     }
 
+    @Operation(summary = "로그인", description = "로그인")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그인 성공 !"),
+            @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(example = "INVALID_HEADER or INVALID_TOKEN")))
+    })
     @GetMapping("/login")
     public RspTemplate<TokenDto> login(@RequestBody @Valid MemberLoginReqDto memberLoginReqDto) {
         MemberLoginResDto memberLoginResDto = memberService.login(memberLoginReqDto);
@@ -33,6 +48,11 @@ public class MemberController {
         return new RspTemplate<>(HttpStatus.OK, "로그인", tokens);
     }
 
+    @Operation(summary = "프로필", description = "회원 프로필 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원 프로필 조회 성공 !"),
+            @ApiResponse(responseCode = "401", description = "인증 실패", content = @Content(schema = @Schema(example = "INVALID_HEADER or INVALID_TOKEN")))
+    })
     @GetMapping("/profile")
     public MemberInfoResDto getMemberInfo(Principal principal) {
         return memberService.getMemberInfo(principal);

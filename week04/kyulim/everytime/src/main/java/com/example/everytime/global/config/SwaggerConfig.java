74e716b3cc9e4709889ec7db917b45290b0afe1c
ckr8305 <ckr8305@naver.com>
2024-10-04 17:09:48 +0900

    refactor: swagger 로그인 추가

--- week04/kyulim/everytime/src/main/java/com/example/everytime/global/config/SwaggerConfig.java
+++ week04/kyulim/everytime/src/main/java/com/example/everytime/global/config/SwaggerConfig.java
@@ -3,17 +3,26 @@ package com.example.everytime.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    private final String AUTH_TOKEN_HEADER = "Authorization";
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .addSecurityItem(new SecurityRequirement().addList(AUTH_TOKEN_HEADER))
+                .components(new Components()
+                        .addSecuritySchemes(AUTH_TOKEN_HEADER, new SecurityScheme()
+                                .name(AUTH_TOKEN_HEADER)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("Bearer")
+                                .bearerFormat("JWT")));
     }
 
     private Info apiInfo() {
